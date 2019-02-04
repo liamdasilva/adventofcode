f52532a3cbd38da90880328ba5e9a09d26e384e5
Liam Da Silva <liam.dasilva@loblaw.ca>
2019-02-04 17:06:56 -0500

    Day 9 WIP

---
+++ lib/day9/q2.ex
@@ -0,0 +1,19 @@
+defmodule Day9.Q2 do
+  @moduledoc """
+  Documentation for Day 9 Q2.
+  """
+
+  @doc """
+
+
+  Assumptions:  1.
+
+  ## Examples
+
+      iex> Q2.run([])
+      nil
+
+  """
+  def run(input_stream) do
+  end
+end
---
+++ test/day9/day9_test.exs
@@ -0,0 +1,16 @@
+defmodule Day9Test do
+  use ExUnit.Case
+  alias Day9.Q1
+  alias Day9.Q2
+
+  doctest Q1
+  # doctest Q2
+
+  # test "q1 works with my input" do
+  #   assert File.read!("lib/fixtures/day9_input") |> Q1.run() == 36566
+  # end
+
+  # test "q2 works with my input" do
+  #   assert File.read!("lib/fixtures/day9_input") |> Q2.run() == 30548
+  # end
+end
